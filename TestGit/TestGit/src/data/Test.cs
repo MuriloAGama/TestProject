77ca1fb78ff89419e0c48707a44217322ba18e33
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-05-03 22:39:45 -0300

    @MuriloAGama [Task: Criando atributo idade]

--- TestGit/TestGit/src/data/Test.cs
+++ TestGit/TestGit/src/data/Test.cs
@@ -6,8 +6,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-       public string Password { get; set; }
-
+        public string Password { get; set; }
+        public int Age { get; set; }
         
     }
 }
