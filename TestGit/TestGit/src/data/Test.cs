53b32bd08223d67d11178cd69d7443365550d66d
MuriloAGama <mu_riloandrade2@hotmail.com>
2022-05-03 22:17:28 -0300

    @MuriloAGama

---
+++ TestGit/TestGit/src/data/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace TestGit.src.data
+{
+    public class Test
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        
+    }
+}
